9d8d21a9c5113c8b23091895d0277c70ad1ae69b
hamzalamin <hamzalamin80@gmail.com>
2024-12-02 14:27:35 +0100

    feat: Add PostgreSQL database service configuration and Spring Data JPA integration

--- src/main/java/com/wora/ebanking/EBankingApplication.java
+++ src/main/java/com/wora/ebanking/EBankingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EBankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EBankingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EBankingApplication.class, args);
+    }
 
 }
